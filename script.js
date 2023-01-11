78ec6eeda5c8916a6db21c59ef5d066769994b12
futoruma <evgeny.martov@gmail.com>
2023-01-11 22:53:08 +0900

    Made prettier.

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
-let newDateInput = document.querySelector("#dateinput")
-let newExpenseInput = document.querySelector("#expenseinput")
-let newAmountInput = document.querySelector("#amountinput")
+let newDateInput = document.querySelector("#dateinput");
+let newExpenseInput = document.querySelector("#expenseinput");
+let newAmountInput = document.querySelector("#amountinput");
 
-let table = document.querySelector("#table")
+let table = document.querySelector("#table");
 
-let nothingAdded = document.querySelector("#nothingadded")
+let nothingAdded = document.querySelector("#nothingadded");
 
-let addButton = document.querySelector("#addbutton")
+let addButton = document.querySelector("#addbutton");
 
-let table_rows = 0
+let table_rows = 0;
 
 addButton.addEventListener("click", () => {
-    nothingAdded.innerText = ""
-    table_rows++
+  nothingAdded.innerText = "";
+  table_rows++;
 
-    let newDateValue = newDateInput.value
-    let newExpenseValue = newExpenseInput.value
-    let newAmountValue = newAmountInput.value
+  let newDateValue = newDateInput.value;
+  let newExpenseValue = newExpenseInput.value;
+  let newAmountValue = newAmountInput.value;
 
-    newDateInput.value = null
-    newExpenseInput.value = null
-    newAmountInput.value = null
+  newDateInput.value = null;
+  newExpenseInput.value = null;
+  newAmountInput.value = null;
 
-    let newRow = document.createElement("tr")
-    table.append(newRow)
+  let newRow = document.createElement("tr");
+  table.append(newRow);
 
-    let newDateCell = document.createElement("td")
-    newRow.append(newDateCell)
-    newDateCell.append(newDateValue)
+  let newDateCell = document.createElement("td");
+  newRow.append(newDateCell);
+  newDateCell.append(newDateValue);
 
-    let newExpenseCell = document.createElement("td")
-    newRow.append(newExpenseCell)
-    newExpenseCell.append(newExpenseValue)
+  let newExpenseCell = document.createElement("td");
+  newRow.append(newExpenseCell);
+  newExpenseCell.append(newExpenseValue);
 
-    let newAmountCell = document.createElement("td")
-    newRow.append(newAmountCell)
-    newAmountCell.append(newAmountValue)
+  let newAmountCell = document.createElement("td");
+  newRow.append(newAmountCell);
+  newAmountCell.append(newAmountValue);
 
-    let deleteButton = document.createElement("button")
-    deleteButton.innerText = "remove"
-    deleteButton.classList.add("deletebutton")
-    newRow.append(deleteButton)
-    deleteButton.addEventListener("click", () => {
-        table.removeChild(newRow)
-        table_rows--
-        if (table_rows === 0) {
-            nothingAdded.innerText = "No expenses added yet."
-        }
-    })
-
-})
+  let deleteButton = document.createElement("button");
+  deleteButton.innerText = "remove";
+  deleteButton.classList.add("deletebutton");
+  newRow.append(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    table.removeChild(newRow);
+    table_rows--;
+    if (table_rows === 0) {
+      nothingAdded.innerText = "No expenses added yet.";
+    }
+  });
+});
